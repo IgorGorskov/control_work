9afff12703c157b19fb83aab15383e181814a0e6
Горшков Игорь <Igor>
2022-12-31 13:34:12 +0300

    program done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int s = 0;
+string [] array = new string[5]{"hello", "2", "world", ":-)", "1"};
+foreach(string i in array){
+    if (i.Length <= 3) {
+        s++;
+    }
+}
+string [] answer = new string[s];
+int j = 0;
+
+foreach(string i in array){
+    if (i.Length <= 3) {
+        answer[j] = i;
+        j++;
+    }
+
+}
+for (int i = 0; i < answer.GetLength(0); i++){
+    Console.Write($"{answer[i]} ");
+}
